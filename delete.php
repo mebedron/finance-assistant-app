536a71308439104f821e5aeffb390b69de534b1b
mrekun <rekunmaks99@gmail.com>
2026-06-22 18:18:25 +0200

    comments added

--- delete.php
+++ delete.php
@@ -6,6 +6,8 @@ if (isset($_POST['id']) && isset($_SESSION['user_id'])) {
     $id = $_POST['id'];
     $user_id = $_SESSION['user_id'];
 
+    // WAŻNE: usuwamy tylko jeśli id się zgadza I transakcja należy do tego usera
+    // bez "AND user_id = ?" każdy mógłby usunąć cudzą transakcję, po prostu zgadując id
     $stmt = $conn->prepare("DELETE FROM transactions WHERE id = ? AND user_id = ?");
     $stmt->bind_param("ii", $id, $user_id);
 
